c6d534e57b5ae2c3b2797280b91bddec0ce65b64
Bodysite <rinat1kam@mail.ru>
2021-03-05 18:08:36 +0500

    start

---
+++ js/script.js
@@ -0,0 +1,33 @@
+"use strict";
+
+// // Метод reduce
+// const arr1 = [1, 2, 3];
+// const reducer = function(accumulator, currentValue) {
+//     return accumulator + currentValue;
+// };
+// console.log(arr1.reduce(reducer, 0));
+
+
+// // Функция, аналогичная методу reduce
+// const arr = [1, 2, 3];
+// function myReduce(array, callback, initValue) {
+//     let result = initValue;
+//     for (let i = 0; i < array.length; i++) {
+//         result = callback(result, array[i], i, array);
+//     }
+//     return result;
+// }
+// console.log(myReduce(arr, (a, e) => a + e, 0));
+
+
+
+// // Спрашиваем у пользователя ключ (свойство) объекта и выдаем алерт со значением этого свойства
+// let user = {
+//     name: "John",
+//     age: 30
+// };
+// let key = prompt("Что вы хотите узнать о пользователе?", "name");
+//   // доступ к свойству через переменную
+// alert( user[key] ); // John (если ввели "name")
+
+console.log('hello привет');
